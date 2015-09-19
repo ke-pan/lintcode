1b3c5f1405bbafb903ab7e2e3b39f9adcc13418b
ke-pan <ke_pan@yeah.net>
2015-09-19 13:48:16 +0800

    list cycle

---
+++ ListCycle.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+public class ListCycle {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins.
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        while (head != slow.next) {
+            head = head.next;
+            slow = slow.next;
+        }
+
+        return head;
+    }
+}
